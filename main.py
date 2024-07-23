3a4d57f4ba27bfa7bffe130d70ce07d5a15eb3c1
kevin <kevin.tartour@gmail.com>
2024-07-23 15:17:07 +0200

    Add 1st API function get_prof

--- main.py
+++ main.py
@@ -17,15 +17,32 @@ mdp2 = quote_plus(mdp)              #pour recoder en cas de caractères spéciau
 
 URI="mongodb+srv://"+utilisateur+":"+mdp2+"@"+cluster+".mongodb.net/"       #Reconstitution de l'URI
 
-mongo_prof=MongoManager(URI,datab_name,"prof")      #Instanciation pour consulter la collection prof
+mongo_client=MongoManager(URI,datab_name)      #Instanciation pour consulter la collection prof
 
 ###             Prof            ###
 @app.get("/professeurs/", tags=["Professeur"])
 async def get_prof():
     # renvoyer nos données
-    return {"L'ensemble des professeurs est":mongo_prof.list_prof()}
+    return {"L'ensemble des professeurs est":mongo_client.list_prof()}
 
 ###         Eleve               ###
 @app.get("/eleves/", tags=["Eleves"])
 async def get_eleve_par_classe():
-    return {"donnees": mongo_prof.list_prof()}
+    return {"L'ensemble des élèves dans chaque classe est": mongo_client.list_eleve_par_class()}
+
+@app.get("/eleves_par_classe/", tags=["Eleves"])
+async def get_eleve_choix_classe(classe):
+    return {"L'ensemble des élèves dans chaque classe est": mongo_client.list_eleve_choix_class(classe)}
+
+@app.get("/notes par eleve/", tags=["Eleves"])
+async def get_note_eleve(prenom,nom):
+    return {f"note eleve {mongo_client.note_choix_eleve(prenom,nom)}"}
+
+@app.get("/notes par prof/", tags=["Eleves"])
+async def get_note_prof(prenom,nom):
+    return {f"Les élèves de {prenom} {nom} ont eu les notes {mongo_client.note_choix_prof(prenom,nom)}"}
+
+@app.post("/eleves", tags=["Professeur"])
+async def post_lieu(date_saisie,nom_eleve,prenom_eleve,nom_classe,nom_matiere,nom_prof,prenom_prof,nom_trimestre,note,avis,avancement):
+    return mongo_client.fill_notes(date_saisie,nom_eleve,prenom_eleve,nom_classe,nom_matiere,nom_prof,prenom_prof,nom_trimestre,note,avis,avancement)
+
--- mongo_manager.py
+++ mongo_manager.py
@@ -4,7 +4,7 @@ from typing import  List                                #Pour mettre des contrai
 from iteration_utilities import unique_everseen         #Pour dédupliquer un liste
 
 class MongoManager:
-    def __init__(self,URI:str, db_name:str, coll_name):                 # :str une info , pas une restriction
+    def __init__(self,URI:str, db_name:str):                 # :str une info , pas une restriction
         self.__client = MongoClient(URI, server_api=ServerApi ("1"), tls=True)            #tls pour sécurisé le transfert de données, Attribut pour accéder au moteur de la BDD Mongo (ex: compass)
         try:
             ping = self.__client.admin.command({'ping': 1})
@@ -13,12 +13,61 @@ class MongoManager:
             raise Exception("Unable to connect to MongoDB due to the following"
                             " error: ", e)
         self.__db = self.__client[db_name]               #Attribut pour accéder à la base de données en mode dictionnaire ou self.__db = self.__client.get_database(db_name)
-        self.__collection = self.__db[coll_name]        #Attribut pour accéder à la collection aussi possible avec get_collection(coll_name)
+
 
     def list_prof(self):
         try:
+            self.__collection = self.__db["prof"]  # Attribut pour accéder à la collection
             liste_de_prof=list(self.__collection.find({}, {"_id": 0, "nom_prof": 1}))
             liste_de_prof = [d['nom_prof'] for d in liste_de_prof]
             return liste_de_prof
         except Exception as e:
             print(e)
+
+    def list_eleve_par_class(self):
+        try:
+            self.__collection = self.__db["eleve"]  # Attribut pour accéder à la collection
+            liste_eleves = list(self.__collection.find({},{"_id": 0, "nom_eleve": 1, "prenom_eleve": 1, "nom_classe": 1}).sort({"nom_classe":1}))
+            liste_eleves = [d['prenom_eleve']+" "+d["nom_eleve"]+" "+d["nom_classe"] for d in liste_eleves]
+            return liste_eleves
+        except Exception as e:
+            print(e)
+
+    def list_eleve_choix_class(self, classe):
+        try:
+            self.__collection = self.__db["eleve"]  # Attribut pour accéder à la collection
+            liste_eleves = list(self.__collection.find({"nom_classe":classe},{"_id": 0, "nom_eleve": 1, "prenom_eleve": 1, "nom_classe": 1}))
+            liste_eleves = [d['prenom_eleve']+" "+d["nom_eleve"]+" "+d["nom_classe"] for d in liste_eleves]
+            return liste_eleves
+        except Exception as e:
+            print(e)
+
+    def note_choix_eleve(self, prenom, nom):
+        try:
+            self.__collection = self.__db["notes"]  # Attribut pour accéder à la collection
+            liste_notes = list(self.__collection.find({"nom_eleve":nom,"prenom_eleve":prenom},{
+                "_id":0,"prenom_eleve":1,"nom_eleve":1,"nom_matiere":1,"note":1}))
+            liste_notes = [d['prenom_eleve']+" "+d["nom_eleve"]+" "+d["nom_matiere"]+" "+str(d["note"]) for d in liste_notes]
+            return liste_notes
+        except Exception as e:
+            print(e)
+
+    def note_choix_prof(self, prenom, nom):
+        try:
+            self.__collection = self.__db["notes"]  # Attribut pour accéder à la collection
+            liste_notes = list(self.__collection.find({"nom_prof":nom,"prenom_prof":prenom},{
+                "_id":0,"prenom_eleve":1,"nom_eleve":1,"nom_matiere":1,"note":1}))
+            liste_notes = [d['prenom_eleve']+" "+d["nom_eleve"]+" "+d["nom_matiere"]+" "+str(d["note"]) for d in liste_notes]
+            return liste_notes
+        except Exception as e:
+            print(e)
+
+    def fill_notes(self,date_saisie,nom_eleve,prenom_eleve,nom_classe,nom_matiere,nom_prof,prenom_prof,nom_trimestre,note,avis,avancement):
+        try:
+            self.__collection = self.__db["notes"]  # Attribut pour accéder à la collection
+            document= {"date_saisie":date_saisie,"nom_eleve":nom_eleve,"prenom_eleve":prenom_eleve,"nom_classe":nom_classe,"nom_matiere":nom_matiere,"nom_prof":nom_prof,"prenom_prof":prenom_prof,"nom_trimestre":nom_trimestre,"note":int(note),"avis":avis,"avancement":int(avancement)}
+            self.__collection.insert_one(document)  # possible de faire un return pour savoir commment ça s'est passé
+
+            return "documents insérés"
+        except Exception as e:
+            print(f"documents non insérés du à: {e}")
